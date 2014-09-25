336c989f37c61bb33e05aecef218137a342a3fe3
Brandon Hopkins <brhpkns@gmail.com>
2014-09-25 10:45:49 -0400

    Initializes with twitter bearer token

--- account_spider.py
+++ account_spider.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import base64
 from pymongo import MongoClient
 class AccountSpider:
     client = MongoClient('localhost', 27017)
@@ -6,9 +8,18 @@ class AccountSpider:
     collection = db.accounts
     def __init__(self):
        secrets_fo = open('secrets.json','r')
-       secrets = secrets_fo.read()
+       secrets = json.loads(secrets_fo.read())
        secrets_fo.close()
-       print secrets
-     #  r= requests.get('http://docs.python-requests.org/en/latest/')
+       self.bearer_token = self.__get_bearer_token(base64.b64encode(secrets["twitter_token"]))
 
-AccountSpider()
+    def __get_bearer_token(self, twitter_token):
+        headers = {"User-Agent": "Account-Spider", "Authorization": "Basic " + twitter_token,
+                 "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+         }
+        body = "grant_type=client_credentials"
+        r = requests.post('https://api.twitter.com/oauth2/token', headers=headers, data=body)
+        return json.loads(r.text)["access_token"]
+
+
+
+print AccountSpider().bearer_token;
